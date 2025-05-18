e84c12e62d2bd7ced425ebea5362d66d1e8d3869
keysijones <keysi.fernandes@clevertech.biz>
2025-05-18 16:00:40 -0300

    Adds button variants file

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,7 +1,7 @@
 
 import React from "react";
 import { Checkbox } from "@/components/ui/checkbox";
-import { Button } from "@/components/ui/button";
+import { Button } from "@/components/ui/button/button";
 import { Trash2 } from "lucide-react";
 import { Todo } from "@/types/todo";
 import { cn } from "@/lib/utils";
